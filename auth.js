53f992f7c30a1f1599612fe1703a30a6328cd90e
Tamasi <Benjamin.Tamasi@cgg.com>
2019-11-20 15:57:57 +0000

    fix oauth format

--- auth.js
+++ auth.js
@@ -1,25 +1,25 @@
 const getStorefront = (z, bundle) => z.request('https://api.music.apple.com/v1/me/storefront');
 
 const getAccessToken = async (z, {cleanedRequest: {querystring: q}}) => ({
-    access_token: q.code,
-    storefront: q.sf,
+  access_token: q.code,
+  storefront: q.sf,
 });
 
 module.exports = {
-    type: 'oauth2',
+  type: 'oauth2',
+  oauth2Config: {
     authorizeUrl: {
-        url: process.env.OAUTH_URL,
-        params: {
-            client_id: '{{process.env.CLIENT_ID}}',
-            state: '{{bundle.inputData.state}}',
-            redirect_uri: '{{bundle.inputData.redirect_uri}}',
-            response_type: 'code'
-        }
+      url: process.env.OAUTH_URL,
+      params: {
+        client_id: '{{process.env.CLIENT_ID}}',
+        state: '{{bundle.inputData.state}}',
+        redirect_uri: '{{bundle.inputData.redirect_uri}}',
+        response_type: 'code'
+      }
     },
     getAccessToken,
     autoRefresh: false,
-    test: getStorefront,
-    connectionLabel: (z, bundle) => {
-        return "Apple Music";
-    }
+  },  
+  test: getStorefront,
+  connectionLabel: "Apple Music",
 };
