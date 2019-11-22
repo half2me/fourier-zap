b0819f0a169cbff37e6e1364bcd4ee59a44f24f6
Benjamin Tamasi <benjamin.tamasi@cgg.com>
2019-11-22 12:03:34 +0000

    finished playlist resource

--- resources/playlist.js
+++ resources/playlist.js
@@ -1,39 +1,36 @@
 const { baseUrl } = require('../music')
 
-const mapPlaylist = ({json: r}) => ({
-  id: r.id,
-  name: r.attributes.name,
-  dateAdded: r.attributes.dateAdded,
-  description: r.attributes.description.standard,
+const mapPlaylist = p => ({
+  id: p.id,
+  name: p.attributes.name,
+  description: p.attributes.description.standard,
+  dateAdded: p.attributes.dateAdded,
 });
 
 const getPlaylist = (z, {inputData: {id}}) => z
   .request(`${baseUrl}/me/library/playlists/${id}`)
-  .then(mapPlaylist);
+  .then(({json: {data}}) => mapPlaylist(data[0]));
 
 const listPlaylists = (z, bundle) => z
   .request(`${baseUrl}/me/library/playlists`, {params: {limit: 100}})
-  .then(mapPlaylist);
+  .then(({json: {data}}) => data.map(mapPlaylist));
 
-const createPlaylist = (z, bundle) => {
-  return z
-    .request()
-    .then(({json: r}) => ({}));
-};
+const createPlaylist = (z, {inputData: {name, description}}) => z
+  .request(`${baseUrl}/me/library/playlists`, {method: 'POST', body: JSON.stringify({
+    attributes: {name, description}
+  })})
+  .then(({json: {data}}) => mapPlaylist(data[0])); // dateAdded will not be available here
 
 const sample = {
   id: "p.eoGxB3vSJao3Km",
   name: "My amazing playlist",
-  dateAdded: "2019-09-01T19:42:34Z",
   description: "This playlists holds my favourite tracks",
+  dateAdded: "2019-09-01T19:42:34Z",
 };
 
 module.exports = {
   key: 'playlist',
   noun: 'Playlist',
-  // The get method is used by Zapier to fetch a complete representation of a record. This is helpful when the HTTP
-  // response from a create call only return an ID, or a search that only returns a minimuml representation of the
-  // record. Zapier will follow these up with the get() to retrieve the entire object.
   get: {
     display: {
       label: 'Get Playlist',
@@ -42,10 +39,9 @@ module.exports = {
     operation: {
       inputFields: [{ key: 'id', required: true }],
       perform: getPlaylist,
-      sample: sample
+      sample,
     }
   },
-  // The list method on this resource becomes a Trigger on the app. Zapier will use polling to watch for new records
   list: {
     display: {
       label: 'New Playlist',
@@ -53,11 +49,9 @@ module.exports = {
     },
     operation: {
       perform: listPlaylists,
-      sample: sample
+      sample: [sample]
     },
   },
-
-  // The create method on this resource becomes a Write on this app
   create: {
     display: {
       label: 'Create Playlist',
@@ -65,58 +59,33 @@ module.exports = {
     },
     operation: {
       inputFields: [
-        { key: 'name', required: true, type: 'string' },
         {
-          key: 'directions',
+          key: 'name',
           required: true,
-          type: 'text',
-          helpText: 'Explain how should one make the recipe, step by step.'
-        },
-        {
-          key: 'authorId',
-          required: true,
-          type: 'integer',
-          label: 'Author ID'
-        },
-        {
-          key: 'style',
-          required: false,
           type: 'string',
-          helpText: 'Explain what style of cuisine this is.'
-        }
+          label: 'Name',
+          helpText: 'Name of the playlist'
+        },
+        {
+          key: 'description',
+          required: false,
+          type: 'text',
+          label: 'Description',
+          helpText: 'Playlist description which will be shown in the playlist details',
+        },
       ],
-      perform: createRecipe,
-      sample: sample
+      perform: createPlaylist,
+      sample: {
+        ...sample,
+        dateAdded: undefined, // This field is not returned on create
+      }
     }
   },
-  // The search method on this resource becomes a Search on this app
-  search: {
-    display: {
-      label: 'Find Recipe',
-      description: 'Finds an existing recipe by name.'
-    },
-    operation: {
-      inputFields: [{ key: 'name', required: true, type: 'string' }],
-      perform: searchRecipe,
-      sample: sample
-    }
-  },
-
-  // In cases where Zapier needs to show an example record to the user, but we are unable to get a live example
-  // from the API, Zapier will fallback to this hard-coded sample. It should reflect the data structure of
-  // returned records, and have obviously dummy values that we can show to any user.
-  sample: sample,
-
-  // If the resource can have fields that are custom on a per-user basis, define a function to fetch the custom
-  // field definitions. The result will be used to augment the sample.
-  // outputFields: () => { return []; }
-  // Alternatively, a static field definition should be provided, to specify labels for the fields
+  sample,
   outputFields: [
     { key: 'id', label: 'ID' },
-    { key: 'createdAt', label: 'Created At' },
     { key: 'name', label: 'Name' },
-    { key: 'directions', label: 'Directions' },
-    { key: 'authorId', label: 'Author ID' },
-    { key: 'style', label: 'Style' }
+    { key: 'description', label: 'Description' },
+    { key: 'dateAdded', label: 'Date Added' },
   ]
 };
