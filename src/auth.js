fa7aed1c47b7b28338b75588452558b98aa81b69
Benjamin Tamasi <halftome@gmail.com>
2020-04-13 11:33:17 +0100

    lint

--- src/auth.js
+++ src/auth.js
@@ -1,8 +1,8 @@
 const { baseUrl } = require('./music');
 
 const getStorefront = (z, bundle) => {
-  z.console.log(bundle)
-  return z.request(`${baseUrl}/me/storefront`)
+  z.console.log(bundle);
+  return z.request(`${baseUrl}/me/storefront`);
 };
 
 const getAccessToken = async (_z, { cleanedRequest: { querystring: q } }) => ({
